0a72280f310a4e016c276d49b2e7d0726f7dcae4
Lucas <kongus99@gmail.com>
2021-01-17 14:56:27 +0100

    backend

---
+++ src/main/kotlin/com/zoovu/puzzle/PuzzleApplication.kt
@@ -0,0 +1,11 @@
+package com.zoovu.puzzle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PuzzleApplication
+
+fun main(args: Array<String>) {
+	runApplication<PuzzleApplication>(*args)
+}
